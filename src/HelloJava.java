acbfef605126e8251d58cc5562bcb58f69eb1734
TheAutomotiveSwDeveloper <nilesh.p.garud@gmail.com>
2019-08-05 09:09:20 +0530

    Added initial version - Java setup ready

---
+++ src/HelloJava.java
@@ -0,0 +1,8 @@
+/* Nilesh - Created to check the environment settings. */
+public class HelloJava {
+	public static void main(String[] argv)
+	{
+		System.out.println("Hello Java Programmers");
+	}
+
+}
